0c8483795316f139285ccb15bbf97751342ecd26
Vlad Lytvynenko <sir.sagramor@gmail.com>
2021-05-02 17:30:12 +0300

    Chck

--- app.js
+++ app.js
@@ -20,10 +20,49 @@ io.on('connection', function (socket) {
 });
 
 
-app.listen(3000, function () {
+
+app.use((req, res) => res.send('INDEX', { root: __dirname }))
+    .listen(3000, function () {
     console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
 });
 
+//
+// const express = require('express');
+// const socketIO = require('socket.io');
+//
+// const PORT = process.env.PORT || 3000;
+// const INDEX = '/index.html';
+//
+// const app = express()
+//     .listen(PORT, () => console.log(`Listening on ${PORT}`));
+//
+//
+// const io = socketIO({
+//     rejectUnauthorized: false,
+//     wsEngine: app,
+//     allowEIO3: true,
+// });
+//
+//
+// io.on('connection', function (socket) {
+//     console.log('Got new connection');
+//     io.sockets.emit("user-joined", socket.id, io.engine.clientsCount, Object.keys(io.sockets.clients().sockets));
+//
+//     socket.on('signal', (toId, message) => {
+//         console.log('In signal');
+//         io.to(toId).emit('signal', socket.id, message);
+//     });
+//
+//     socket.on("message", function (data) {
+//         console.log('In message');
+//         io.sockets.emit("broadcast-message", socket.id, data);
+//     })
+//
+//     socket.on('disconnect', function () {
+//         console.log('In disconnect');
+//         io.sockets.emit("user-left", socket.id);
+//     })
+// });
 
 // TODO use latest node and update dependencies and make this work on heroku
 // TODO deploy client to the heroku, explore how to do it, maybe use express?
